91304e695797e66c43d4f8d5f638b8750c78a9f6
Jens Schauder <jens@schauderhaft.de>
2024-11-18 12:46:24 +0100

    Fails with 2.7.4

--- src/test/java/de/schauderhaft/hsqldb/issue1725/ReproducerTest.java
+++ src/test/java/de/schauderhaft/hsqldb/issue1725/ReproducerTest.java
@@ -1,13 +1,64 @@
 package de.schauderhaft.hsqldb.issue1725;
 
+import org.hsqldb.jdbc.JDBCDataSource;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class ReproducerTest {
 
+	static final JDBCDataSource ds = new JDBCDataSource();
+
+	@BeforeAll
+	static void setUp() {
+		ds.setURL("jdbc:hsqldb:mem:test");
+
+		try (Connection connection = ds.getConnection()) {
+
+			connection.prepareStatement("create table dummy( id int, text varchar(20))").execute();
+
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
 	@Test
-	void failure() {
-		fail("Just for testing");
+	@Order(100)
+	void one() {
+		insert();
+	}
+
+	@Test
+	@Order(200)
+	void two() {
+		insert();
+	}
+
+
+	private void insert() {
+		try (Connection connection = ds.getConnection()) {
+
+			PreparedStatement preparedStatement = connection.prepareStatement("insert into dummy(id, text) values (?, ?)", Statement.RETURN_GENERATED_KEYS);
+			preparedStatement.setInt(1, 4711);
+			preparedStatement.setString(2, "text1");
+			preparedStatement.executeUpdate();
+
+			try (ResultSet resultSet = preparedStatement.getGeneratedKeys()) {
+				while (resultSet.next()) {
+					System.out.println("rs element");
+				}
+			}
+		} catch (SQLException ex) {
+			throw new RuntimeException(ex);
+		}
 	}
 }
