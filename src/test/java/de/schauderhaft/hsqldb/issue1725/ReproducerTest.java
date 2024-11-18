4a5e79bc8b284ceda8b91dcfa0cfe2c2ee58ea1c
Jens Schauder <jens@schauderhaft.de>
2024-11-18 12:21:42 +0100

    initial commit

---
+++ src/test/java/de/schauderhaft/hsqldb/issue1725/ReproducerTest.java
@@ -0,0 +1,13 @@
+package de.schauderhaft.hsqldb.issue1725;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ReproducerTest {
+
+	@Test
+	void failure() {
+		fail("Just for testing");
+	}
+}
